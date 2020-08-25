a41bf56083f617c0648d7abe74566e73187ab056
Phillip Lee <59787386+phillipinseoul@users.noreply.github.com>
2020-08-25 09:14:58 -0700

    Update the client.
    - Change inet_addr("localhost") to inet_addr("127.0.0.1").
    - Understand the difference between "localhost" and "127.0.0.1"

--- multi_client.c
+++ multi_client.c
@@ -1,5 +1,6 @@
 /*
  * File name: multi_client.c
+ * Compile code: gcc multi_client -o client -pthread
  */
 
 #include <stdio.h>
@@ -34,7 +35,8 @@ void *clientThread(void *arg){
 	serverAddr.sin_port = htons(7799);
 
 	// Set IP address to localhost
-	serverAddr.sin_addr.s_addr = inet_addr("localhost");
+	// serverAddr.sin_addr.s_addr = inet_addr("localhost");
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
 	// Connect the socket to the server using the address
