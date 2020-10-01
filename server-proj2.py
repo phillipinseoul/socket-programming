2a4b7846ebd8e39f453a43ad9bb8beb53be20d60
phillip <phillip9807@gmail.com>
2020-10-02 02:24:50 +0900

    server commit

--- server-proj2.py
+++ server-proj2.py
@@ -1,6 +1,9 @@
 import socket
 import threading
 
+# Header size: 64
+HEADER = 64
+
 # Define a port to run the program on.
 PORT = 5050
 # Put in my computer's IP address
@@ -8,7 +11,37 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 # Binding socket
 ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+
+# Handle communication between server & client
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+        print(f"[{addr}] {msg}")
+    conn.close()
+
+# Listen
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+print("[STARTING] server is starting...")
+start()
+
