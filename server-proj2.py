39a76f38adbf96e570a57362f79798b40d6fc811
phillip <phillip9807@gmail.com>
2020-10-02 02:00:27 +0900

    binding

--- server-proj2.py
+++ server-proj2.py
@@ -6,6 +6,9 @@ PORT = 5050
 # Put in my computer's IP address
 # SERVER = "192.168.25.22"
 SERVER = socket.gethostbyname(socket.gethostname())
+# Binding socket
+ADDR = (SERVER, PORT)
 
-print(SERVER)
-print(socket.gethostname())
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
