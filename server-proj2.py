4e3193d2a4dd49284087ddd7e44490dd4d1b5c3a
phillip <phillip9807@gmail.com>
2020-10-02 11:10:08 +0900

    send from server to client

--- server-proj2.py
+++ server-proj2.py
@@ -30,7 +30,8 @@ def handle_client(conn, addr):
             if msg == DISCONNECT_MESSAGE:
                 connected = False
             print(f"[{addr}] {msg}")
-            
+            conn.send("Msg received".encode(FORMAT))
+
     conn.close()
 
 # Listen
